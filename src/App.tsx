4201dd82b161f72ccae8820dd2bf7a1a7de76ce4
Amir Asaad <amiroasaad@gmail.com>
2022-12-27 10:50:14 +0800

    Feat: Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import './App.css';
 import About from './sections/About/components/About';
 import Contacts from './sections/Contacts/component/Contacts';
+import Footer from './sections/Footer/components/Footer';
 import Greeting from './sections/Greeting/components/Greeting';
 import Projects from './sections/Projects/components/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <Greeting />
+    <div>
+      <div className="App">
+        <Greeting />
+        <hr className="mb-8" />
+        <About />
+        <hr className="mb-8" />
+        <Projects />
+        <hr className="mb-8" />
+        <Contacts />
+      </div>
       <hr className="mb-8" />
-      <About />
-      <hr className="mb-8" />
-      <Projects />
-      <hr className="mb-8" />
-      <Contacts />
+      <Footer />
     </div>
   );
 }
---
+++ src/sections/Footer/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="mb-8 pl-4">
+      <a
+        className="white--text"
+        href="icons8.com"
+        target="_blank"
+      >
+        Icons used are from <span className="text-underline">icons8</span>.
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
